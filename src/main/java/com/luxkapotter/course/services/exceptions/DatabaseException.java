17cddead09d771c4cbc030f5f9db738c5db23cb5
Luccas Souza <emaildocastro.adm01@gmail.com>
2022-10-28 15:05:51 -0300

    criando tratamento de exceção do delete

---
+++ src/main/java/com/luxkapotter/course/services/exceptions/DatabaseException.java
@@ -0,0 +1,8 @@
+package com.luxkapotter.course.services.exceptions;
+
+public class DatabaseException extends RuntimeException{
+    
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+}
